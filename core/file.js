60978ec6cbb2fc56e965bba1624252af7d0cc0ba
8-shen <664905885@qq.com>
2017-09-13 15:57:33 +0800

    test

--- core/file.js
+++ core/file.js
@@ -3,16 +3,17 @@
  * @author 巴神
  * 2017/6/23
  */
-var fs = require('fs'),
-    zlib = require("zlib");
+
+const fs = require('fs'),
+  zlib = require("zlib");
 
 /**
  * 文件写入类，直接写入静态资源
  * 
  * @param {any} req 
  */
-function writer(req){
-    
+function writer(req) {
+
 }
 
 /**
@@ -20,17 +21,17 @@ function writer(req){
  * 
  * @param {any} req 
  */
-function reader(req,cb){
-  var stream=fs.createReadStream(path);
-  stream.on('data',function(chunk){
-    cb && cb('pedding',chunk);
+function reader(req, cb) {
+  var rs = fs.createReadStream(path);
+  rs.on('data', function (chunk) {
+    cb && cb('pedding', chunk);
   });
-  stream.on('end',function(chunk){
-    cb && cb('resolved',chunk);
+  rs.on('end', function (chunk) {
+    cb && cb('resolved', chunk);
   });
 }
 
 
-module.exports=function(type, req){
-    type ? reader(req):writer(req);
+module.exports = function (type, req) {
+  type ? reader(req) : writer(req);
 };
